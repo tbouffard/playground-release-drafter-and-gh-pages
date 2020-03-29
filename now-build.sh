2ec9bc542756924f4d4d164ebd7bea0494386b40
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-03-29 10:30:02 +0200

    Deploy: add dedicated bash script for Now deployment (fix error on gh-pages branch)

---
+++ now-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Hack for the Now GitHub integration
+# Currently (2020-03-29) there is no way to filter branch used to deploy: all branches are deployed
+# This is an issue for the 'gh-pages' branch which has no package.json
+# So use a dedicated bash script in 'master' to run npm and in 'gh-pages' to only copy static content
+rm -rf dist/
+mkdir -p dist/
+
+cp index.html dist/
+cp -R demo/ dist/
