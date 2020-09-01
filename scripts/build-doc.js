97fc17ee289c39a75e1295c3df4bb48985860a39
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-09-01 22:18:50 +0200

    wip copy images

--- scripts/build-doc.js
+++ scripts/build-doc.js
@@ -1,19 +1,31 @@
 const fs = require('fs');
-
+const fse = require('fs-extra')
 const Asciidoctor = require('asciidoctor');
-
-console.warn('called');
+// const path = require('path');
 
 const asciidoctor = Asciidoctor()
 const html = asciidoctor.convert(fs.readFileSync('docs/index.adoc'), {
   base_dir: 'docs',
-  to_file: 'build/docs/index.html',
+  to_file: '../build/docs/index.html',
   standalone: true,
   mkdirs: true,
-  attributes: { showtitle: true, icons: 'font' }
-// to_dir
+  safe: 'unsafe', // needed because we want to generate the html outside of the directory that stores the sourc files
 });
 
+// cp -R ${doc_input}/architecture/images ${doc_output}/images
+// cp -R ${doc_input}/images ${doc_output}
+
+copyFileSync('docs/architecture/images', 'build/docs/images');
+copyFileSync('docs/images', 'build/docs/images');
 
 
-// var doc = asciidoctor.convertFile('/path/to/file.adoc', { to_file: true, standalone: true, mkdirs: true })
+
+function copyFileSync(relPathToSourceFile, relPathToDestinationDirectory, destinationFileName) {
+  // const directoryPath = -.join(__dirname, relPathToDestinationDirectory);
+  fse.ensureDirSync(relPathToDestinationDirectory);
+  // fs.mkdirSync(directoryPath, { recursive: true });
+  fse.copySync(relPathToSourceFile, relPathToDestinationDirectory);
+  // fs.copyFileSync(path.join(__dirname, relPathToSourceFile), directoryPath);
+  // fs.copyFileSync(path.join(__dirname, relPathToSourceFile), path.join(directoryPath, destinationFileName));
+}
+
